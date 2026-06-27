b616a2d6a2db85ded3452231e34946c09b5f855b
Mikayel <158308669+SleepyXm@users.noreply.github.com>
2026-06-27 18:46:06 +0100

    movement

--- app/services/registration.ts
+++ app/services/registration.ts
@@ -1,11 +1,15 @@
 export const BASE_URL = import.meta.env.VITE_API_BASE_URL + "/api";
+const API_KEY = import.meta.env.VITE_xage47282;
 
 if (!BASE_URL) throw new Error("VITE_API_BASE_URL is not set");
 
 export async function joinWaitlist(email: string) {
   const res = await fetch(`${BASE_URL}/waitlist`, {
     method: "POST",
-    headers: { "Content-Type": "application/json" },
+    headers: { 
+      "Content-Type": "application/json",
+      "x-api-key": API_KEY,
+    },
     body: JSON.stringify({ email }),
   });
 
@@ -25,6 +29,10 @@ export async function joinWaitlist(email: string) {
 export async function getCount() {
   const res = await fetch(`${BASE_URL}/count`, {
     method: "GET",
+    headers: { 
+      "Content-Type": "application/json",
+      "x-api-key": API_KEY,
+    },
   });
 
   if (!res.ok) throw new Error("Failed to retrieve count");
